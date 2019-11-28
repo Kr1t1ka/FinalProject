5003afb13db53ff2d92bc285fe9a9426b699a29b
Kr1t1ka <poloposhta@gmail.com>
2019-11-28 18:36:17 +0300

    test

--- app/src/main/java/bonch/dev/shool/fianlproject/Activity/SignUpActivity.kt
+++ app/src/main/java/bonch/dev/shool/fianlproject/Activity/SignUpActivity.kt
@@ -1,12 +1,12 @@
-package bonch.dev.shool.fianlproject
+package bonch.dev.shool.fianlproject.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.sign_up_activity)
     }
 }
---
+++ app/src/main/java/bonch/dev/shool/fianlproject/Activity/StartActivity.kt
@@ -0,0 +1,12 @@
+package bonch.dev.shool.fianlproject.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start_activity)
+    }
+}
