3ea0413f5646ab5f8e483e91f68d1e46f86aaccc
meogol <avs99.98@gmail.com>
2019-12-01 16:34:11 +0300

    Добавил авторизацию

--- app/src/main/java/bonch/dev/shool/fianlproject/Activity/StartActivity.kt
+++ app/src/main/java/bonch/dev/shool/fianlproject/Activity/StartActivity.kt
@@ -4,12 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import bonch.dev.shool.fianlproject.R
+import com.google.firebase.auth.FirebaseAuth
 
 class StartActivity : AppCompatActivity() {
 
     private lateinit var InputButton : Button
     private lateinit var RegistrationButton : Button
+
+    private lateinit var etPassword : EditText
+    private  lateinit var  etEmail : EditText
+
+    private  var mAuth : FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +26,47 @@ class StartActivity : AppCompatActivity() {
         InputButton = findViewById(R.id.input_button)
         RegistrationButton = findViewById(R.id.regestration_button)
 
+        etEmail = findViewById(R.id.editTextTextEmailAddress)
+        etPassword = findViewById(R.id.editTextTextPassword)
+
+        mAuth = FirebaseAuth.getInstance()
+
         InputButton.setOnClickListener {
-            startActivity(Intent(this, MainActivity().javaClass))
-            finish()
+            logInUser(etEmail.text.toString(), etPassword.text.toString())
         }
 
         RegistrationButton.setOnClickListener {
             startActivity(Intent(this, SignUpActivity().javaClass))
             finish()
         }
+    }
+
+
+    /**
+     * Метод проверяет правильность ввода и наличие пользователя в БД. Перебрасывает на mainActivity
+     * если проверки пройдены
+     */
+    private fun logInUser(email : String, password : String){
+
+        if(email == "" || password == ""){
+            Toast.makeText(
+                this, "Не все поля заполнены", Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
+        mAuth!!.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){task ->
+                if(task.isSuccessful){
+                    val intent = Intent(this,  MainActivity().javaClass)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    startActivity(intent)
+                }
+                else{
+                    Toast.makeText(this, "Ошиббка авторизации",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+
     }
 }
