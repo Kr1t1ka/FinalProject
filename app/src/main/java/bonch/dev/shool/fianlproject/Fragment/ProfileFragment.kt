251c58aada80c07fd53afeeb68706fbb3cd19b5c
meogol <avs99.98@gmail.com>
2019-12-09 03:49:07 +0300

    Теперь отображается имя пользователя

--- app/src/main/java/bonch/dev/shool/fianlproject/Fragment/ProfileFragment.kt
+++ app/src/main/java/bonch/dev/shool/fianlproject/Fragment/ProfileFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import bonch.dev.shool.fianlproject.Activity.MainActivity
 import bonch.dev.shool.fianlproject.Activity.StartActivity
 
@@ -21,7 +22,7 @@ class ProfileFragment : Fragment() {
     private lateinit var exitButton : ImageButton
     private lateinit var user: User
     private lateinit var addCurses: Button
-
+    private lateinit var tvName: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,26 +34,28 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         var view = inflater.inflate(R.layout.fragment_profile, container, false)
+        tvName = view.findViewById(R.id.user_name)
         user = (context as MainActivity).user
         exitButton = view.findViewById(R.id.button2)
         addCurses = view.findViewById(R.id.add_cuses)
+
         val admin : String = user.isAdmin
+
         if(admin == "1"){
             addCurses.setVisibility(View.VISIBLE)
         }else{
             addCurses.setVisibility(View.GONE)
         }
+
+        tvName.text = user.UserName
+
         exitButton.setOnClickListener {
             FirebaseAuth.getInstance().signOut();
 
             startActivity(Intent(context,  StartActivity().javaClass))
         }
 
-        /**
-         * TODO: из класса user в тул бар транслировать имя пользователя
-         * и еще доделать профиль надо
-         *
-         */
+
 
         return view
     }
